e95c898a5b86f04ebbf7ab7af3a1c44caf9a72b5
澁谷悠大 <univer405@gmail.com>
2022-02-12 14:41:57 +0900

    conplete chapter

--- front/components/Chapter.vue
+++ front/components/Chapter.vue
@@ -9,20 +9,18 @@
       <span class="chapter-title" @click="c">{{tmpTitle}}</span>
     </template>
     <p class="ml-auto mr-3">理解度 100%</p>
-    <!--
-    <b-btn @click="addChapter">
+    <b-btn class="plus-btn mr-3" @click="addSection">
       <svg
       xmlns="http://www.w3.org/2000/svg"
       width="16"
       height="16"
       fill="currentColor"
-      class="bi bi-plus-circle align-self-center mr-3"
+      class="bi bi-plus-circle align-self-center"
       viewBox="0 0 16 16">
         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
       </svg>
     </b-btn>
-    -->
     <b-btn class="trash-btn" @click="deleteChapter">
       <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -123,10 +121,10 @@ export default ({
 .chapter-title{
   font-size: 20px;
 }
-.bi-trash{
+.bi-trash,.bi-plus-circle{
   color: black;
 }
-.trash-btn{
+.trash-btn,.plus-btn{
   background-color: transparent;
 }
 </style>
